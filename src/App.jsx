f27d4e5437009c1a04deda550d116c602920c88f
caslumes <caslumes@outlook.com>
2025-08-11 16:14:35 -0300

    showing users info in a table, delete user working, login form disappearing after successful login

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import axios from "axios";
 import CustomButton from "./components/CustomButton";
 import LoginForm from "./components/LoginForm";
+import { Trash } from "lucide-react";
 
 function App() {
     const [users, setUsers] = useState([]);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const apiUrl = "http://localhost:8080/api";
     const loginUrl = `${apiUrl}/login`;
@@ -37,6 +39,15 @@ function App() {
             })
             .then((res) => {
                 accessToken.current = res.data.access_token;
+                axios.defaults.headers.common[
+                    "Authorization"
+                ] = `Bearer ${accessToken.current}`;
+
+                setIsLoggedIn(true);
+
+                usernameInputRef.current.value = "";
+                passwordInputRef.current.value = "";
+
                 console.log("Login successful");
             })
             .catch((err) => {
@@ -45,7 +56,6 @@ function App() {
                     return;
                 }
                 console.error("Error when logging in: ", err.response.data);
-                return;
             });
     };
 
@@ -55,29 +65,74 @@ function App() {
 
     const fetchUsers = async () => {
         axios
-            .get(usersUrl, {
-                headers: {
-                    Authorization: `Bearer ${accessToken.current}`,
-                },
-            })
+            .get(usersUrl)
             .then((res) => setUsers(res.data))
             .catch((err) => console.log(err.response.data));
     };
 
+    const handleDeleteUser = (userId) => {
+        deleteUser(userId);
+    };
+
+    const deleteUser = async (userId) => {
+        console.log(`Deleting user with id ${userId}.`);
+        axios
+            .delete(`${usersUrl}/${userId}`)
+            .then(() => fetchUsers())
+            .catch((err) => {
+                if (err.response === undefined) {
+                    console.error(err);
+                    return;
+                }
+                if (err.response.status === 403) {
+                    console.error("Você não tem permissão para isso!");
+                }
+            });
+    };
+
     return (
         <>
-            <div className="flex flex-col items-center gap-5">
-                <LoginForm
-                    handleLogin={handleLogin}
-                    usernameInputRef={usernameInputRef}
-                    passwordInputRef={passwordInputRef}
-                />
+            <div className="flex flex-col items-center gap-5 pt-5">
+                {isLoggedIn ? null : (
+                    <LoginForm
+                        handleLogin={handleLogin}
+                        usernameInputRef={usernameInputRef}
+                        passwordInputRef={passwordInputRef}
+                    />
+                )}
+
                 <CustomButton onClick={handleGetUsersButtonClick}>
                     MOSTRAR USUARIOS
                 </CustomButton>
-                {users.map((user) => (
-                    <p key={user.id}>{user.name}</p>
-                ))}
+                <table className="w-[50%]">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Nome de usuário</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-center">
+                        {users.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>
+                                    <button
+                                        onClick={() =>
+                                            handleDeleteUser(user.id)
+                                        }
+                                        className="cursor-pointer hover:bg-black/25 p-1 rounded-full"
+                                    >
+                                        <Trash />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </>
     );
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,24 @@
 import CustomButton from "./CustomButton";
 import CustomInput from "./CustomInput";
 
-function LoginForm({handleLogin, usernameInputRef, passwordInputRef}) {
-    return <form
-        onSubmit={handleLogin}
-        className="m-auto w-[50%] pt-5 flex flex-col gap-2"
-    >
-        <CustomInput ref={usernameInputRef} placeholder={"Nome de Usuário"} />
-        <CustomInput ref={passwordInputRef} placeholder={"Senha"} type="password" />
-        <CustomButton type="submit">ENTRAR</CustomButton>
-    </form>;
+function LoginForm({ handleLogin, usernameInputRef, passwordInputRef }) {
+    return (
+        <form
+            onSubmit={handleLogin}
+            className="m-auto w-[50%] flex flex-col gap-2"
+        >
+            <CustomInput
+                ref={usernameInputRef}
+                placeholder={"Nome de Usuário"}
+            />
+            <CustomInput
+                ref={passwordInputRef}
+                placeholder={"Senha"}
+                type="password"
+            />
+            <CustomButton type="submit">ENTRAR</CustomButton>
+        </form>
+    );
 }
 
 export default LoginForm;
